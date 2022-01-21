619182964cb5cb224b185dc317f07f9980675bc8
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-20 22:23:12 -0800

    Completed Main Assignments and 2 bonus questions

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,143 @@
-console.log('Simple debugging example running.')
-debugger
+// console.log('Simple debugging example running.')
 
-let x = 99
-debugger
-console.log(x)
+// let x = 99
+// console.log(x)
+
+function numPointsScored (name) {
+    let game = gameObject() 
+    
+    for(let team in game) {
+        let players = game[team]['players']
+        for(let player in players) {
+            if(player === name) {
+                return players[player]['points']
+            }
+        }
+    }
+}
+
+function shoeSize (name) {
+    let game = gameObject()
+
+    for(let team in game) {
+        let players = game[team]['players']
+        for(let player in players) {
+            if(player === name) {
+                return players[player]['shoe']
+            }
+        }
+    }
+}
+
+function teamColors (tName) {
+    let game = gameObject()
+
+    for( let team in game) {
+        let gTeam = game[team]
+        if(gTeam['teamName'] === tName) {
+            return gTeam['colors']
+        }
+    }
+}
+
+function teamNames () {
+    let game = gameObject()
+    let arrTeams = []
+    for(let team in game) {
+        arrTeams.push(game[team]['teamName'])
+    }
+    return arrTeams
+}
+
+function playerNumbers (tName) {
+    let game = gameObject()
+    let pNums = [];
+    for(let team in game) {        
+        if(game[team]['teamName'] === tName) {
+            let players = game[team]['players']
+            for(let player in players) {
+                pNums.push(players[player]['number'])
+            }
+        }         
+    }
+    return pNums
+}
+
+function playerStats (playerName) {
+    let game = gameObject()
+    for(let team in game) {
+        let players = game[team]['players']
+        for(let player in players) {
+            if(player === playerName) {
+                return players[player]
+            }
+        }
+    }
+}
+
+function bigShoeRebounds () {
+    let game = gameObject()
+    let maxShoeSize = 0
+    let rebounds = 0
+    for(let team in game) {
+        let players = game[team]['players'];
+        for(let player in players) {
+            if(players[player]['shoe'] > maxShoeSize) {
+                maxShoeSize = players[player]['shoe']
+                rebounds = players[player]['rebounds']
+            }
+
+        }
+    }
+    return rebounds
+}
+
+
+/**************** BONUS QUESTIONS ****************/
+
+function mostPointsScored () {
+    let game = gameObject()
+    let maxPointsScored = 0
+    let playerName = ''
+    for(let team in game) {
+        let players = game[team]['players']
+        for(let player in players) {
+            if(players[player]['points'] > maxPointsScored) {
+                maxPointsScored = players[player]['points']
+                playerName = player
+            }
+        }
+    }
+    return playerName
+}
+
+function winningTeam () {
+    let game = gameObject()
+    let teams = []
+
+    for(let team in game) {
+        
+        //every time loop goes to different team, it creates currentTeam object
+        //and sets current teamName as value of new objects's teamName 
+        let currentTeam = {teamName: game[team]['teamName'], teamScore: 0}
+        let players = game[team]['players']
+        
+        //add all players scores in same team to new object's teamScore
+        for(let player in players) {
+            currentTeam.teamScore += players[player]['points']            
+        }
+        teams.push(currentTeam)
+    }
+
+    let winner = ''
+    let maxPoints = 0
+    for(let team in teams) {
+        if(teams[team].teamScore > maxPoints){
+            winner = teams[team].teamName
+            maxPoints = teams[team].teamScore
+        }
+    }
+    return winner
+}
+
+console.log(winningTeam())
